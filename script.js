196b71befa0f1c4b95473a94fc9ba19266ce49de
PedroSmaxY <pedrohnovais@outlook.com>
2023-07-21 21:41:17 -0300

    Improved  readability by using English variable.

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-function gerarFatores() {
-  let valor = window.document.querySelector("#txtn");
-  let numero = Number(valor.value);
-  let lista = window.document.querySelector("div#res ul");
-  lista.innerHTML = "";
-  let primo = 2;
+function generateFactors() {
+  let value = window.document.querySelector("#txtn");
+  let number = Number(value.value);
+  let list = window.document.querySelector("div#res ul");
+  list.innerHTML = "";
+  let prime = 2;
 
-  if (numero <= 0 || numero > 999999999999) {
-    window.alert("ERRO Valor impossível!");
+  if (number <= 0 || number > 999999999999) {
+    window.alert("ERROR: Impossible value!");
   } else {
     do {
-      if (numero % primo == 0) {
-        numero /= primo;
+      if (number % prime === 0) {
+        number /= prime;
         let item = document.createElement("li");
-        item.textContent = `${primo}`;
-        lista.appendChild(item);
-      } else if (primo > 2) {
-        primo += 2;
+        item.textContent = `${prime}`;
+        list.appendChild(item);
+      } else if (prime > 2) {
+        prime += 2;
       } else {
-        primo++;
+        prime++;
       }
-    } while (numero !== 1);
+    } while (number !== 1);
   }
 }
